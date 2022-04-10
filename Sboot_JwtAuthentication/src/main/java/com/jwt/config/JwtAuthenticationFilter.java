ae3800b996780495368204ee2a5164c973e4314e
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2022-04-11 01:00:14 +0530

    jwt Utility

--- Sboot_JwtAuthentication/src/main/java/com/jwt/config/JwtAuthenticationFilter.java
+++ Sboot_JwtAuthentication/src/main/java/com/jwt/config/JwtAuthenticationFilter.java
@@ -81,6 +81,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
+	            // After setting the Authentication in the context, we specify that the current user is authenticated. So it passes the Spring Security Configurations successfully.  
 	            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 			}
         }
